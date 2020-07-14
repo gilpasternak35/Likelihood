53e51357b12d466ee9caa30bd2307ef194d29aa0
gilpasternak35 <gilpasternak35@gmail.com>
2020-07-14 10:05:30 -0700

    Initial splitting of prophet into .py files

---
+++ ProphetFiles/venv/group_and_build_time_table.py
@@ -0,0 +1,5 @@
+import numpy as np
+def group_and_build_time_table(truncated_data):
+    grouped_counts = truncated_data.value_counts()
+    prophet_df = pd.DataFrame({'ds':grouped_counts.index,'y':np.log10(grouped_counts.values)})
+    return prophet_df
---
+++ ProphetFiles/venv/truncate_training_set.py
@@ -0,0 +1,3 @@
+
+def truncate_training_set(training_set):
+    return trainingSet.replace(minute=0, second=0,  microsecond=0)
